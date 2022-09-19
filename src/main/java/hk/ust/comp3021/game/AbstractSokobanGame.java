882a4fef485c47e25c61491995ad401906ca5107
owen-hwlee <hwolee@connect.ust.hk>
2022-09-20 04:47:16 +0800

    Implemented AbstractSokobanGame except undo

--- src/main/java/hk/ust/comp3021/game/AbstractSokobanGame.java
+++ src/main/java/hk/ust/comp3021/game/AbstractSokobanGame.java
@@ -1,6 +1,7 @@
 package hk.ust.comp3021.game;
 
 import hk.ust.comp3021.actions.*;
+import hk.ust.comp3021.entities.*;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -19,10 +20,10 @@ public abstract class AbstractSokobanGame implements SokobanGame {
      * For example when the user specified to exit the game or the user won the game.
      */
     protected boolean shouldStop() {
-        // TODO
+        // DONE
 
         // User specified to exit
-        if (false) {    // TODO: implement specify to exit
+        if (this.state.mostRecentAction instanceof Exit) {
             return true;
         }
 
@@ -40,22 +41,76 @@ public abstract class AbstractSokobanGame implements SokobanGame {
      */
     protected ActionResult processAction(@NotNull Action action) {
         // TODO
+        // All updates to GameState after each Action are performed in this function
+
+        this.state.mostRecentAction = action;
         return switch (action) {
             case InvalidInput invalidInput -> {
-                yield new ActionResult.Failed(action, "Invalid Input.");
+                yield new ActionResult.Failed(invalidInput, "Invalid Input.");
             }
             case Exit exit -> {
-                // TODO: handle exit
-                yield new ActionResult.Success(action);
+                yield new ActionResult.Success(exit);
             }
             case Undo undo -> {
-                // TODO: handle undo
-                yield new ActionResult.Success(action);
+                if (this.state.getUndoQuota().isPresent() && this.state.getUndoQuota().get() == 0) {
+                    // Undo quota used up
+                    yield new ActionResult.Failed(undo, "You have run out of your undo quota.");
+                } else {
+                    // Undo to last checkpoint, unless nothing moved
+                    if (false) {        // TODO: check boxes moved
+                        this.state.undo();
+                    }
+                    yield new ActionResult.Success(undo);
+                }
             }
             case Move move -> {
-                // TODO: handle move
-                // if can't move, use ActionResult.Failed()
-                yield new ActionResult.Success(action);
+                int playerId = move.getInitiator();
+                Position playerPosition = this.state.getPlayerPositionById(playerId);
+
+                if (playerPosition == null) {
+                    // PlayerID does not exist
+                    yield new ActionResult.Failed(move, "Player not found.");
+                }
+
+                // Process Move
+                // Players can only move one box in each Move
+                // Check movable by confirming nextPosition contains either own Box or Empty
+                // If nextPosition contains own Box, check nextNextPosition contains Empty then can move
+                // If nextPosition contains Empty, can move
+                Position nextDestination = move.nextPosition(playerPosition);
+                yield switch (this.state.getEntity(nextDestination)) {
+                    case Empty empty -> {
+                        // Player can move to nextDestination
+                        this.state.move(playerPosition, nextDestination);
+                        yield new ActionResult.Success(move);
+                    }
+                    case Box box -> {
+                        if (box.getPlayerId() != playerId) {
+                            // Pushed other players' Box
+                            yield new ActionResult.Failed(move, "You cannot move other players' boxes.");
+                        }
+                        // Check nextNextDestination for space to push own Box
+                        Position nextNextDestination = move.nextPosition(nextDestination);
+                        switch (this.state.getEntity(nextNextDestination)) {
+                            case Empty empty -> {
+                                // Can push box
+                                this.state.move(nextDestination, nextNextDestination);
+                                this.state.move(playerPosition, nextDestination);
+                                yield new ActionResult.Success(move);
+                            }
+                            case null, default -> {
+                                // Blocked, cannot push box
+                                yield new ActionResult.Failed(move, "Failed to push the box.");
+                            }
+                        }
+                    }
+                    case Wall wall -> {
+                        yield new ActionResult.Failed(move, "You hit a wall.");
+                    }
+                    case Player player -> {
+                        yield new ActionResult.Failed(move, "You hit another player.");
+                    }
+                };
             }
         };
     }
